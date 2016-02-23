b142c2b4cfffac45e9aa30d64ea278329451d2b6
Peter Schüllermann <peter.schuellermann@gmail.com>
2016-02-23 20:33:58 +0100

    update gulpfile.js
    pizzeria is now resized

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
+var imageResize = require('gulp-image-resize');
 
 
 gulp.task('useref', function(){
@@ -32,14 +33,29 @@ gulp.task('images', function(){
         .pipe(gulp.dest('dist/img'))
 });
 
+gulp.task('pizzeria', function(){
+    return gulp.src('src/img/pizzeria.jpg')
+
+
+
+        // Caching images that ran through imagemin
+        .pipe(cache(imagemin({
+            interlaced: true,
+            optimizationLevel : 7
+        })))
+        .pipe(imageResize({ width: 400 }))
+        .pipe(gulp.dest('dist/img'))
+});
+
 
 gulp.task('clean:dist', function() {
     return del.sync('dist');
-})
+});
 
 gulp.task('build', function (callback) {
     runSequence('clean:dist',
-        ['useref', 'images'],
+        ['useref', 'images', 'pizzeria'],
         callback
     )
-})
+});
+
