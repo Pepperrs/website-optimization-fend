fdc3f9ccc4d8eb7fbd07698328e7bc1f14189e49
Peter Schüllermann <peter.schuellermann@gmail.com>
2016-02-23 17:48:09 +0100

    update all
    moved unmodified assets into src folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require('gulp');
+
+var clean = require('gulp-clean');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 var imageop = require('gulp-image-optimization');
 
+var bases = {
+    app: 'app/',
+    dist: 'dist/',
+};
 
-gulp.task('default', function() {
-    // place code for your default task here
-});
-
+var paths = {
+    scripts: ['js/**/*.js', '!js/libs/**/*.js'],
+    libs: ['js/libs/jquery/dist/jquery.js', 'js/libs/underscore/underscore.js', 'scripts/backbone/backbone.js'],
+    styles: ['css/**/*.css'],
+    html: ['*.html'],
+    images: ['img/**/*.png', 'img/**/*.jpg'],
+    extras: ['crossdomain.xml', 'humans.txt', 'manifest.appcache', 'robots.txt', 'favicon.ico'],
+};
 
 
 gulp.task('images', function(cb) {
@@ -15,3 +29,56 @@ gulp.task('images', function(cb) {
         interlaced: true
     })).pipe(gulp.dest('public/images')).on('end', cb).on('error', cb);
 });
+
+
+
+
+// Delete the dist directory
+gulp.task('clean', function() {
+    return gulp.src(bases.dist)
+        .pipe(clean());
+});
+
+// Process scripts and concatenate them into one output file
+gulp.task('scripts', ['clean'], function() {
+    gulp.src(paths.scripts, {cwd: bases.app})
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest(bases.dist + 'scripts/'));
+});
+
+// Imagemin images and ouput them in dist
+gulp.task('imagemin', ['clean'], function() {
+    gulp.src(paths.images, {cwd: bases.app})
+        .pipe(imagemin())
+        .pipe(gulp.dest(bases.dist + 'images/'));
+});
+
+// Copy all other files to dist directly
+gulp.task('copy', ['clean'], function() {
+    // Copy html
+    gulp.src(paths.html, {cwd: bases.app})
+        .pipe(gulp.dest(bases.dist));
+
+    // Copy styles
+    gulp.src(paths.styles, {cwd: bases.app})
+        .pipe(gulp.dest(bases.dist + 'styles'));
+
+    // Copy lib scripts, maintaining the original directory structure
+    gulp.src(paths.libs, {cwd: 'app/**'})
+        .pipe(gulp.dest(bases.dist));
+
+    // Copy extra html5bp files
+    gulp.src(paths.extras, {cwd: bases.app})
+        .pipe(gulp.dest(bases.dist));
+});
+
+// A development task to run anytime a file changes
+gulp.task('watch', function() {
+    gulp.watch('app/**/*', ['scripts', 'copy']);
+});
+
+// Define the default task as a sequence of the above tasks
+gulp.task('default', ['clean', 'scripts', 'imagemin', 'copy']);
